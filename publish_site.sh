991eabbb43a7f0c6f1aff722ff9b6bc960ecc902
David Denton <denton.david@gmail.com>
2016-06-05 09:28:44 +0100

    dave

---
+++ publish_site.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo Publishing site...
+set -e
+
+npm run cleanSite
+rm -rf /tmp/co
+git clone git@github.com:blackswanindustries/blackswanindustries.github.io.git /tmp/co
+cd /tmp/co
+cd -
+npm run makeSite
+rm -rf /tmp/co/*
+mv -f target/www/* /tmp/co
+cp -f public/static/* /tmp/co
+cd /tmp/co
+git add *
+git commit -m "releasing site"
+git push origin master
+cd -
